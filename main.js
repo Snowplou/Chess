08c3c4b1d108b974fed90e5baaba0a21b726950a
Snowplou <Snowplou01@gmail.com>
2023-01-20 09:07:21 -0800

    started remaking available moves system

--- main.js
+++ main.js
@@ -1,40 +1,44 @@
 // Create board
-let board = {}
-for (let i = 1; i <= 64; i++) {
-    board[i] = { piece: "empty" }
+let board = []
+for (let i = 0; i < 8; i++) {
+    board[i] = []
+    for (let j = 0; j < 8; j++) {
+        board[i].push("empty")
+    }
 }
-board[1] = { piece: "wrook" }
-board[2] = { piece: "wknight" }
-board[3] = { piece: "wbishop" }
-board[4] = { piece: "wqueen" }
-board[5] = { piece: "wking" }
-board[6] = { piece: "wbishop" }
-board[7] = { piece: "wknight" }
-board[8] = { piece: "wrook" }
-board[9] = { piece: "wpawn" }
-board[10] = { piece: "wpawn" }
-board[11] = { piece: "wpawn" }
-board[12] = { piece: "wpawn" }
-board[13] = { piece: "wpawn" }
-board[14] = { piece: "wpawn" }
-board[15] = { piece: "wpawn" }
-board[16] = { piece: "wpawn" }
-board[57] = { piece: "brook" }
-board[58] = { piece: "bknight" }
-board[59] = { piece: "bbishop" }
-board[60] = { piece: "bqueen" }
-board[61] = { piece: "bking" }
-board[62] = { piece: "bbishop" }
-board[63] = { piece: "bknight" }
-board[64] = { piece: "brook" }
-board[49] = { piece: "bpawn" }
-board[50] = { piece: "bpawn" }
-board[51] = { piece: "bpawn" }
-board[52] = { piece: "bpawn" }
-board[53] = { piece: "bpawn" }
-board[54] = { piece: "bpawn" }
-board[55] = { piece: "bpawn" }
-board[56] = { piece: "bpawn" }
+
+board[0][0] = "wrook"
+board[1][0] = "wknight"
+board[2][0] = "wbishop"
+board[3][0] = "wqueen"
+board[4][0] = "wking"
+board[5][0] = "wbishop"
+board[6][0] = "wknight"
+board[7][0] = "wrook"
+board[0][1] = "wpawn"
+board[1][1] = "wpawn"
+board[2][1] = "wpawn"
+board[3][1] = "wpawn"
+board[4][1] = "wpawn"
+board[5][1] = "wpawn"
+board[6][1] = "wpawn"
+board[7][1] = "wpawn"
+board[0][7] = "brook"
+board[1][7] = "bknight"
+board[2][7] = "bbishop"
+board[3][7] = "bqueen"
+board[4][7] = "bking"
+board[5][7] = "bbishop"
+board[6][7] = "bknight"
+board[7][7] = "brook"
+board[0][6] = "bpawn"
+board[1][6] = "bpawn"
+board[2][6] = "bpawn"
+board[3][6] = "bpawn"
+board[4][6] = "bpawn"
+board[5][6] = "bpawn"
+board[6][6] = "bpawn"
+board[7][6] = "bpawn"
 
 // Display board
 for (let i = 0; i < 8; i++) {
@@ -45,17 +49,18 @@ for (let i = 0; i < 8; i++) {
         let square = document.createElement("div")
         square.id = 64 - (-j + (i * 8)) - 7
         square.className = "square"
-        if ((Number(square.id) + i) % 2 == 0) {
-            square.classList.add("black")
-        }
-        else {
+        if ((j + i) % 2 == 0) {
             square.classList.add("white")
         }
+        else {
+            square.classList.add("black")
+        }
 
-        if (board[square.id].piece != "empty") {
+        if (board[j][7 - i] != "empty") {
             let pic = document.createElement("img")
-            pic.src = "pictures/" + board[square.id].piece + ".png"
+            pic.src = "pictures/" + board[j][7 - i] + ".png"
             pic.id = "pic" + square.id
+            pic.className = "piece"
             square.appendChild(pic)
         }
 
@@ -67,57 +72,82 @@ for (let i = 0; i < 8; i++) {
 let turn = "white"
 let wcastle = [true, true]
 let bcastle = [false, false]
+let wtorpedo = [true, true, true, true, true, true, true, true]
+let btorpedo = [true, true, true, true, true, true, true, true]
 
 // Get possible moves
-for(let i = 1; i <= 64; i++){
+for (let i = 1; i <= 64; i++) {
     let square = document.getElementById(i)
     square.addEventListener("click", () => {
         squareClicked(square.id);
     })
 }
 
-function checkValid(piece, square){
+function checkValid(piece, square) {
     return true
 }
 
-function squareClicked(clickedSquare){
+function squareClicked(clickedSquare) {
     let square = document.getElementById(clickedSquare)
     let squareNumber = Number(square.id)
     let piece = square.children
-    if(!piece.length) return
+    if (!piece.length) return
     piece = piece[0].src.split("pictures/")[1].split(".png")[0]
+
+    let y = Math.floor((squareNumber - 1) / 8)
+    let x = Math.ceil(squareNumber - 8 * y) - 1
+
     let moves = []
-    if(piece == "wpawn" || piece == "bpawn"){
-        moves = [8]
+    if (piece == "wpawn" || piece == "bpawn") {
+        if ((turn == "white" && wtorpedo[x]) || (turn == "black" && btorpedo[x])) {
+            moves = [[0, 1], [0, 2]]
+        }
+        else {
+            moves = [[0, 1]]
+        }
     }
-    else if(piece == "wrook" || piece == "brook"){
-        moves = [-56, -48, -40, -32, -24, -16, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 16, 24, 32, 40, 48, 56]
+    else if (piece == "wrook" || piece == "brook") {
+        for (let i = x + 1; i <= 7; i++) {
+            if(board[i][y] != "empty"){
+                break
+            }
+            moves.push([i - x, 0])
+        }
+        for (let i = x - 1; i >= 0; i--) {
+            if(board[i][y] != "empty"){
+                break
+            }
+            moves.push([i - x, 0])
+        }
+        for (let i = y + 1; i <= 7; i++) {
+            if(board[x][i] != "empty"){
+                break
+            }
+            moves.push([0, i - y])
+        }
+        for (let i = y - 1; i >= 0; i--) {
+            if(board[x][i] != "empty"){
+                break
+            }
+            moves.push([0, i - y])
+        }
     }
-    else if(piece == "wknight" || piece == "bknight"){
+    else if (piece == "wknight" || piece == "bknight") {
         moves = [-17, -15, -10, -6, 6, 10, 15, 17]
     }
-    else if(piece == "wbishop" || piece == "bbishop"){
+    else if (piece == "wbishop" || piece == "bbishop") {
         moves = [-63, -54, -49, -45, -42, -36, -35, -28, -27, -21, -18, -14, -9, -7, 7, 9, 14, 18, 21, 27, 28, 35, 36, 42, 45, 49, 54, 63]
-    }else if(piece == "wqueen" || piece == "bqueen"){
+    } else if (piece == "wqueen" || piece == "bqueen") {
         moves = [-56, -48, -40, -32, -24, -16, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 16, 24, 32, 40, 48, 56, -63, -54, -49, -45, -42, -36, -35, -28, -27, -21, -18, -14, -9, -7, 7, 9, 14, 18, 21, 27, 28, 35, 36, 42, 45, 49, 54, 63]
     }
-    else if(piece == "wking" || piece == "bking"){
+    else if (piece == "wking" || piece == "bking") {
         moves = [-9, -8, -7, -1, 1, 7, 8, 9]
     }
 
-    // Calculate move border
-    let down = Math.floor((squareNumber - 1) / 8)
-    let up = 7 - down
-    let left = Math.ceil(squareNumber - 8 * down) - 1
-    let right = 7 - left
-
     for(let i = 0; i < moves.length; i++){
-        if(squareNumber + moves[i] >= 1 && squareNumber + moves[i] <= 64){
-            let horizontal
-            console.log(horizontal, vertical)
-            let dot = document.createElement("span")
-            dot.className = "dot"
-            document.getElementById(squareNumber + moves[i]).appendChild(dot)
-        }
+        let availableSquare = document.getElementById(squareNumber + moves[i][0] + (moves[i][1] * 8))
+        availableSquare.classList.remove("white")
+        availableSquare.classList.remove("black")
+        availableSquare.classList.add("available")
     }
 }
